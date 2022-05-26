8a7ede19b957cf2f75f2ba15acf018f9b5734963
khalid <khankhalid003@gmail.com>
2022-05-26 15:03:59 +0530

    all

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function test() {
   let m = document.getElementById("minuts");
   let s = document.getElementById("seconds");
 
-  d.getHours() < 10
-    ? (h.innerHTML = "0" + d.getHours())
+
+  let formate=d.getHours();
+  (formate > 12)?formate=formate % 12:formate=formate;
+
+  formate < 10
+    ? (h.innerHTML = "0" + formate)
     : (h.innerHTML = d.getHours());
   d.getMinutes() < 10
-    ? (m.innerHTML = "0" + d.getMinutes())
-    : (m.innerHTML = d.getMinutes());
+    ? (m.innerHTML = " : "+"0"+ d.getMinutes())
+    : (m.innerHTML = " : "+d.getMinutes());
   d.getSeconds() < 10
-    ? (s.innerHTML = "0" + d.getSeconds())
-    : (s.innerHTML = +d.getSeconds());
+    ? (s.innerHTML = " :"+"0"+d.getSeconds())
+    : (s.innerHTML =" : "+d.getSeconds());
 }
 setInterval(test, 1000);
 let d= new Date();
